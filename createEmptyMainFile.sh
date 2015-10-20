a2186b8431033020f606d81211d28b05dcb35ee2
Joel Gomez <joel.gomez@email.ucr.edu>
2015-10-20 11:35:30 -0700

    fixed echo newline on hammer server

--- createEmptyMainFile.sh
+++ createEmptyMainFile.sh
@@ -5,6 +5,6 @@ if [ -e main.cc ]
 fi
 touch main.cc
 cat joel_861238902.txt >> main.cc
-echo "\n" >> main.cc
+echo  >> main.cc
 echo "    int main(int argc, const char** argv)" >> main.cc
 echo "        {}" >> main.cc
