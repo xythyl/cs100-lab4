44bdd7750073275de1f817a1776f87cf5c619976
Joel Gomez <joel.gomez@email.ucr.edu>
2015-10-20 11:34:28 -0700

    working shell script for empty main file

---
+++ createEmptyMainFile.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ -e main.cc ]
+    then
+        rm main.cc
+fi
+touch main.cc
+cat joel_861238902.txt >> main.cc
+echo "\n" >> main.cc
+echo "    int main(int argc, const char** argv)" >> main.cc
+echo "        {}" >> main.cc
